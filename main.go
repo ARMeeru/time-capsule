6370f701b082a721445ae100c5f519a6bdc4e326
Asifur Rahaman Meeru <asif@skillsdb.com>
2024-10-04 16:45:44 +0600

    fix: default port if not specified in .env

--- main.go
+++ main.go
@@ -27,8 +27,14 @@ func main() {
 	routes.InitRoutes(router)
 
 	// Start the server
-	port := ":" + config.GetEnv("APP_PORT")
-	log.Printf("Server is running on http://localhost%s", port)
+	port := config.GetEnv("APP_PORT")
+	if port == "" {
+		port = ":8080"
+		log.Printf("APP_PORT not specified. Using default port%s", port)
+	} else {
+		port = ":" + port
+		log.Printf("Server is running on http://localhost%s", port)
+	}
 	err := router.Run(port)
 	if err != nil {
 		log.Fatalf("Failed to start the server: %v", err)
